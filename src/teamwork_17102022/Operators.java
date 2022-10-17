faf91bc156782f3d777448d9432c6d0f9be433cf
serhat-project <sucar661@gmail.com>
2022-10-17 14:47:11 +0300

    yeni operators eklendi.

---
+++ src/teamwork_17102022/Operators.java
@@ -0,0 +1,67 @@
+package teamwork_17102022;
+
+import java.util.Scanner;
+
+public class Operators {
+    public static void main(String[] args) {
+//        int num1, num2, sum;
+//
+//        num1 =46;
+//        num2= 90;
+//        sum= num1 +num2;
+//        System.out.println("Toplam değer: " + sum);
+
+
+//        double celsius, fahr;
+//
+//        Scanner input =  new Scanner(System.in);
+//
+//        System.out.println("Derece değeri giriniz");
+//
+//        celsius = input.nextDouble();
+//
+//        fahr = (celsius * 1.8) + 32;
+//
+//        System.out.println("Fahreneit değeriniz: " + fahr);
+
+//        double miles, km;
+//
+//        Scanner input =  new Scanner(System.in);
+//
+//        System.out.println("Miles değeri giriniz");
+//
+//        miles = input.nextDouble();
+//
+//        km = miles / 0.62;
+//
+//        System.out.println("Kilometre değeri: " + km);
+
+//        double r, perimeter, area;
+//
+//        Scanner input =  new Scanner(System.in);
+//
+//        System.out.println("r değeri giriniz");
+//
+//        r = input.nextDouble();
+//
+//        perimeter = 2 * r * Math.PI;
+//        area = Math.pow(r, 2) * Math.PI;
+//
+//        System.out.printf("Cevre değeri: " + "%5.3f", perimeter);
+//        System.out.println("");
+//        System.out.printf("Alan değeri: " + "%5.3f", area);
+
+//        int num1= 10, num2=4 ;
+//        double result = num1/num2;
+//        System.out.println(result);
+
+        int num1= 10, num2=4 ;
+        double result = (double) num1/num2);
+        System.out.println(result);
+
+
+
+
+
+    }
+}
